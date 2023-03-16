878d35087a6ffa2d6e39ce7051e6d4994f7b3225
Aayush <aayush.130702@gmail.com>
2023-03-16 15:19:12 +0530

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row min-h-screen justify-center items-center">
+      <ButtonGroup
+        disableElevation
+        variant="contained"
+        aria-label="Disabled elevation buttons"
+      >
+        <Button>One</Button>
+        <Button>Two</Button>
+      </ButtonGroup>
     </div>
   );
 }
