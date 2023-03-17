0463b6fb62c5d8a7f69ec4bc3013bf62684a1a38
Aayush <aayush.130702@gmail.com>
2023-03-17 17:34:34 +0530

    Index Updated

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(() => console.log('Connected to MongoDB cluster'))
-  .catch(err => console.error('Error connecting to MongoDB cluster', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -52,5 +52,5 @@ app.get("/data", async (req, res) => {
 
 // Start the server
 
-app.listen(process.env.PORT, () => console.log('Server started on port 3000'));
+app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
 
