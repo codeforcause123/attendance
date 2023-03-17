c5382ad3e34b2ee66304c01bbabf5104a9dc6068
Aayush <aayush.130702@gmail.com>
2023-03-17 18:30:19 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-
+import { useState, useEffect } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch(
+        "https://attendancebackend.onrender.com/data"
+      );
+      const json = await response.json();
+      if (response.ok) {
+        setData(json);
+      }
+    };
+    fetchdata();
+  }, []);
+  console.log(data);
   return (
     <div className="flex flex-row min-h-screen justify-center items-center">
       <ButtonGroup
