f7b7d6d918ee86012a3c296fc301f070b294c6b7
Aayush <aayush.130702@gmail.com>
2023-03-20 10:52:33 +0530

    Updated Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useState, useEffect } from "react";
+import { Skeleton } from "@mui/material";
 function App() {
   const [data, setData] = useState([]);
+  const [att,Setatt] = useState([]);
+
   useEffect(() => {
     const fetchdata = async () => {
       const response = await fetch(
@@ -12,6 +15,8 @@ function App() {
       const json = await response.json();
       if (response.ok) {
         setData(json);
+        const dataArray = Object.values(json[0].att[0]);
+        Setatt(dataArray);
       }
     };
     fetchdata();
@@ -19,14 +24,16 @@ function App() {
   console.log(data);
   return (
     <div className="flex flex-row min-h-screen justify-center items-center">
-      <ButtonGroup
-        disableElevation
-        variant="contained"
-        aria-label="Disabled elevation buttons"
-      >
-        <Button>One</Button>
-        <Button>Two</Button>
-      </ButtonGroup>
+      {att.map((item, index) => {
+        return (
+          <div key={index}>
+            <h2>{item.Course}</h2>
+            <p>Conducted {item.Conducted}</p>
+            <p>Attended {item.Attended}</p>
+            <p>Attendance {item.Attendance}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
