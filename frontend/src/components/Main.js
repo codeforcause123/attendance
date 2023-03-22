a363a1143fcc4c2d72ecfaa72858b95724380a13
Aayush <aayush.130702@gmail.com>
2023-03-22 18:35:42 +0530

    Made Responsive

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -7,16 +7,16 @@ export default function Main(props) {
     AOS.init({ duration: 2000 });
   }, []);
   return (
-    <div className="justify-center bg-gradient-to-r from-teal-200 to-lime-200 px-40 py-4">
+    <div className="justify-center bg-gradient-to-r from-teal-200 to-lime-200 sm:px-16 lg:px-48 py-4">
       {props.att.map((item, index) => {
         return (
-          <div className="w-full mx-8 my-8" key={index}>
+          <div className="sm:w-fit lg:w-full mx-8 my-8" key={index}>
             <div
               className="xt-card rounded-2xl text-gray-900 xt-links-default bg-stone-100 my-4"
               data-aos="fade-up"
               data-aos-anchor-placement="top-bottom"
             >
-              <div className="p-7 sm:p-9 text-base content-start ">
+              <div className="p-3 sm:p-9 text-base">
                 <div className="text-3xl pb-2 font-semibold">
                   {item.Course || <Skeleton animation="wave" />}
                 </div>
@@ -28,7 +28,7 @@ export default function Main(props) {
                     Attended: {item.Attended}
                   </p>
                   <p className="text-blue-600 text-xl">
-                    Attendance: {item.Attendance}
+                    Attendance: {item.Attendance}%
                   </p>
                 </div>
               </div>
