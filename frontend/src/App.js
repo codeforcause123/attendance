688e387d56bf0e17ab0ff3b6f92d3d7dd37a9fec
Aayush <aayush.130702@gmail.com>
2023-03-22 10:25:42 +0530

    Added Aos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { Skeleton } from "@mui/material";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 function App() {
   const [att, setAtt] = useState([]);
 
@@ -23,24 +23,7 @@ function App() {
   return (
     <div className="bg-black">
       <Header />
-      <div className="justify-center bg-gradient-to-r from-teal-200 to-lime-200 px-40 py-4">
-        {att.map((item, index) => {
-          return (
-            <div className="w-full mx-8 my-8" key={index}>
-              <div className="xt-card rounded-2xl text-gray-900 xt-links-default bg-stone-100 my-4">
-                <div className="p-7 sm:p-9 text-base">
-                  <div className="text-3xl pb-2 font-semibold">{item.Course || <Skeleton animation="wave"/>}</div>
-                  <div className="pl-2">
-                    <p className="text-orange-600 text-xl">Conducted: {item.Conducted}</p>
-                    <p className="text-green-600 text-xl">Attended: {item.Attended}</p>
-                    <p className="text-blue-600 text-xl">Attendance: {item.Attendance}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Main att={att} />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Skeleton } from "@mui/material";
+export default function Main(props) {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+  return (
+    <div className="justify-center bg-gradient-to-r from-teal-200 to-lime-200 px-40 py-4">
+      {props.att.map((item, index) => {
+        return (
+          <div className="w-full mx-8 my-8" key={index}>
+            <div
+              className="xt-card rounded-2xl text-gray-900 xt-links-default bg-stone-100 my-4"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <div className="p-7 sm:p-9 text-base content-start ">
+                <div className="text-3xl pb-2 font-semibold">
+                  {item.Course || <Skeleton animation="wave" />}
+                </div>
+                <div className="pl-2">
+                  <p className="text-orange-600 text-xl">
+                    Conducted: {item.Conducted}
+                  </p>
+                  <p className="text-green-600 text-xl">
+                    Attended: {item.Attended}
+                  </p>
+                  <p className="text-blue-600 text-xl">
+                    Attendance: {item.Attendance}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
