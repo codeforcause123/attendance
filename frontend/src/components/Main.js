e4653be2a8f08e5ffc6faa1d230aa2e7e38185df
Aayush <aayush.130702@gmail.com>
2023-03-24 15:00:17 +0530

    Fonts Updated

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -9,20 +9,20 @@ export default function Main(props) {
   }, []);
   const n = 13;
   return (
-    <div className="justify-center bg-gradient-to-r from-teal-200 to-lime-200 sm:px-16 lg:px-48 py-4">
+    <div className="justify-center bg-gradient-to-b from-rose-500 to-indigo-700 sm:px-16 lg:px-48 py-4">
       {props.att.length
         ? props.att.map((item, index) => {
             return (
               <div className="sm:w-fit lg:w-full mx-8 my-8" key={index}>
                 <div
-                  className="xt-card rounded-2xl text-gray-900 xt-links-default bg-stone-100 my-4"
+                  className="xt-card rounded-2xl text-white xt-links-default bg-black my-4"
                   data-aos="fade-up"
                   data-aos-anchor-placement="top-bottom"
                 >
                   <div className="p-3 sm:p-9 text-base">
                     <div
                       className="text-3xl pb-2 font-semibold"
-                      style={{ fontFamily: "Source Sans Pro" }}
+                      style={{ fontFamily: "Montserrat" }}
                     >
                       {item.Course || <Skeleton animation="wave" />}
                     </div>
